964651088546050ee171d25f483a970f8b158a28
Brian Cho <chobrian@gmail.com>
2012-06-01 20:34:30 -0500

    Fixed compile error on previous commit

--- hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/KillPreemptor.java
+++ hadoop-mapreduce-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-resourcemanager/src/main/java/org/apache/hadoop/yarn/server/resourcemanager/scheduler/capacity/KillPreemptor.java
@@ -1,26 +1,5 @@
 package org.apache.hadoop.yarn.server.resourcemanager.scheduler.capacity;
 
-import java.util.List;
-
 public class KillPreemptor extends CSPreemptor {
-
-  @Override
-  public void reclaimCapacity() {
-    logQueueStatus(root);
-  }
-  
-  private void logQueueStatus(CSQueue queue) {
-    LOG.info("(bcho2) "+queue.getQueuePath()+
-        " utilization "+queue.getUtilization()+
-        " capacity "+queue.getCapacity());
-    List<CSQueue> children = queue.getChildQueues();
-    if (children == null) { // LeafQueue
-      // TODO get info about applications, not just print
-      ((LeafQueue)queue).printApplicationResourceInfo();
-    } else {
-      for (CSQueue child : children) { // ParentQueue
-        logQueueStatus(child);
-      }
-    }
-  }
+  // Ignore (for now)
 }
