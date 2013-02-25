a70d9647a62656368f74b6aa698b9264013695fc
Tej Chajed <tchajed+github@gmail.com>
2013-02-24 23:44:19 -0600

    adding json marshal/unmarshal to estimate

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,23 +1,36 @@
 # Module for utilities braodly useful for experimentation utlities
 
 from __future__ import print_function
-import math
+import json
+from math import sqrt
 
 class Estimate:
   """ Track an estimated parameter with confidence measure. """
-  def __init__(self, x):
-    self.sum = x
-# sum of squared error
-    self.sse = 0.0
-    self.n = 1
+  def __init__(self, *args, **kwargs):
+    """ Initialize with a value or with json=string. """
+    if len(args) >= 1:
+      self.sum = 0
+      self.sse = 0.0
+      self.n = 0
+      for v in args:
+        self.add(v)
+    else:
+      o = json.loads(kwargs['json'])
+      self.sum = o['sum']
+      self.sse = o['sse']
+      self.n = o['n']
   def mean(self):
     return self.sum / self.n
   def stddev(self):
-    return math.sqrt(self.sse / (self.n - 1))
+    return sqrt(self.sse / (self.n - 1))
   def add(self, v):
     oldmean = self.mean()
     self.sum += v
     self.n += 1
     self.sse += (v - oldmean) * (v - self.mean())
-
-
+  def marshal(self):
+    return json.dumps({
+      "sum": self.sum,
+      "sse": self.sse,
+      "n": self.n,
+      })
