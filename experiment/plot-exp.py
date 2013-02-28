57943ff00bb399493f8e250d754d488564cfe564
Tej Chajed <tchajed+github@gmail.com>
2013-02-28 10:33:37 -0600

    use full precision in csv output for plotting

--- experiment/plot-exp.py
+++ experiment/plot-exp.py
@@ -19,9 +19,9 @@ exp, results = experiment.loadResults(args.results)
 # header row
 csv_data = exp.details["param"] + ",margin0,margin1\n"
 for param, run in sorted(results.items()):
-  csv_data += (exp.details["param_f"] % param) + ","
+  csv_data += ("%f" % param) + ","
   margins = [margin_hat.mean() for margin_hat in run["margins"]]
-  csv_data += ",".join(["%0.0f" % (margin/1e3) for margin in margins])
+  csv_data += ",".join(["%0.2f" % (margin/1e3) for margin in margins])
   csv_data += "\n"
 
 if args.title is None:
