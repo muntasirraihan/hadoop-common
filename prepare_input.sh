289bee9b853eae580c9ebbe7f6e89e0a4de78e4e
Tej Chajed <tchajed@illinois.edu>
2013-03-16 14:09:06 -0600

    prepare supports natjam + bec clusters

--- prepare_input.sh
+++ prepare_input.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 clear
 
+EXPECTED_ARGS=1
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: ./prepare_input <clustername>"
+    echo "cluster_name = natjam | bec"
+    exit 1
+fi
+
 VERSION=0.23.3
 
-HADOOP_HOME=$HOME/hadoop/hadoop-$VERSION-SNAPSHOT
-HADOOP_SOURCE=$HOME/natjam/hadoop-common
+CLUSTER_NAME=$1
+
+HADOOP_HOME=/mnt/hadoop/hadoop-$VERSION-SNAPSHOT
+HADOOP_SOURCE=/proj/ISS/scheduling
 
 # generate random input
-$HADOOP_HOME/bin/hadoop jar $HADOOP_SOURCE/HDFSWrite.jar org.apache.hadoop.examples.HDFSWrite -conf $HADOOP_SOURCE/conf/randomwriter_conf.xsl -Dmapreduce.job.queuename=high workGenInputSmall
+$HADOOP_HOME/bin/hadoop jar $HADOOP_SOURCE/HDFSWrite.jar org.apache.hadoop.examples.HDFSWrite -conf $HADOOP_SOURCE/conf_$CLUSTER_NAME/randomwriter_conf.xsl -Dmapreduce.job.queuename=high workGenInputSmall
